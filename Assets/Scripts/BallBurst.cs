dfa3a2cf6a392a79f50e618f7a12cc2c705b41db
Daniel C <63077752+dcrep@users.noreply.github.com>
2021-05-03 21:09:40 -0400

    AddBurstFixAudio

---
+++ Assets/Scripts/BallBurst.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBurst : MonoBehaviour
+{
+
+    public ParticleSystem ps;
+    public void PlayBurst()
+    {
+        ps.Play();
+    }
+}
