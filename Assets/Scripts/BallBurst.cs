331be48d8c594b1174a404113ffa56bc357f6391
Daniel C <63077752+dcrep@users.noreply.github.com>
2021-05-10 22:00:18 -0400

    CompletenMerge

--- Assets/Scripts/BallBurst.cs
+++ Assets/Scripts/BallBurst.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class BallBurst : MonoBehaviour
 {
 
-    public ParticleSystem ps;
+    public ParticleSystem ps1, ps2, ps3, ps4;
+    private int idx = 0;
     public void PlayBurst()
     {
-        ps.Play();
+        // Rotate
+        switch(idx)
+        {
+            case 0:
+                ps1.Play();
+                break;
+            case 1:
+                ps2.Play();
+                break;
+            case 2:
+                ps3.Play();
+                break;
+            default:
+                ps4.Play();
+                idx = -1;   // increment below resets to 0
+                break;
+        }
+        idx++;
     }
 }
